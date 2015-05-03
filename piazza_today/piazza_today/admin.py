2cec545b4c0e700e4a4a7d70bcb24f58c169d419
Scott Brenstuhl <brenstsr@miamioh.edu>
2015-05-03 14:32:44 +0200

    initial push

---
+++ piazza_today/piazza_today/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Snitch, Offline
+
+# Register your models here.
+admin.site.register(Snitch)
+admin.site.register(Offline)
---
+++ piazza_today/piazza_today/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Snitch(models.Model):
+    pass
+
+class Offline(models.Model):
+    pass
