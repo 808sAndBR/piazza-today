18ba5c5e59b0127fe76763444085f75d06158da9
Scott Brenstuhl <brenstsr@miamioh.edu>
2015-05-05 05:15:22 -0700

    updated html

--- piazza_today/views.py
+++ piazza_today/views.py
@@ -7,13 +7,20 @@ from .querys import Clocks
 def home_page(request):
     lunch = Meal.objects.filter(day = date.today).filter(which_meal = 1)[0]
     dinner = Meal.objects.filter(day = date.today).filter(which_meal = 2)[0]
-    lunch_food = Food.objects.filter(meal__meal=str(lunch))[:2]
-    dinner_food = Food.objects.filter(meal__meal=str(dinner))[:2]
+    lunch_food = Food.objects.filter(meal__meal=str(lunch))[:7]
+    dinner_food = Food.objects.filter(meal__meal=str(dinner))[:7]
     current_time = datetime.now()
     pa_time = Clocks().pa_clock()
     lv_time = Clocks().lv_clock()
     header_date = Clocks().head_date()
-    return render(request, 'piazza_today/home_page.html', {'pa_time':pa_time, 'lv_time':lv_time, 'header_date':header_date, 'lunch':lunch, 'dinner':dinner, 'lunch_food':lunch_food, 'dinner_food':dinner_food})
+    events = TeamEvent.objects.filter(event_date__gt=date.today)[:5]
+    snitch = Snitch.objects.latest('win_date')
+    vacation = Offline.objects.filter(date_returning__gte=date.today).filter(date_leaving__lte=date.today).order_by('date_returning')
+    gamenight_winner = Gamenight.objects.latest('win_date')
+    return render(request, 'piazza_today/home_page.html', {'pa_time':pa_time, 
+        'lv_time':lv_time, 'header_date':header_date, 'lunch':lunch, 'dinner':dinner, 
+        'lunch_food':lunch_food, 'dinner_food':dinner_food, 'events':events, 'snitch':snitch, 
+        'vacation':vacation, 'gamenight_winner':gamenight_winner})
 
 def dev_page(request):
     lunch = Meal.objects.filter(day = date.today).filter(which_meal = 1)[0]
