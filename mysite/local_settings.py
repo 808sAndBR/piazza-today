065098a6eea2427a64a19e8ebfd6ced82ff62548
Scott Brenstuhl <brenstsr@miamioh.edu>
2015-05-04 23:37:15 -0700

    Localdb fix

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,15 +1,15 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# DATABASES = {
-#         'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'piazzatodaydb',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'piazzatodaydb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 DEBUG = True
